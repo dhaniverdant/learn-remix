5d9615c929e4154bb98acc7b60bce611d0495b65
dhaniverdant <dhaniverdant23@gmail.com>
2024-01-17 15:30:56 +0800

    active link styling

--- app/root.tsx
+++ app/root.tsx
@@ -8,10 +8,10 @@ export const links: LinksFunction = () => [
 
 import {
   Form,
-  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -78,18 +78,29 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? (
-                        <span>★</span>
-                      ) : null}
-                    </Link>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {/* <Link to={`contacts/${contact.id}`}> */}
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? (
+                          <span>★</span>
+                        ) : null}
+                      {/* </Link> */}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
