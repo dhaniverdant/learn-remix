a97b8d32d6fd834d73649ea4de13677b04cc14eb
dhaniverdant <dhaniverdant23@gmail.com>
2024-02-01 15:22:01 +0800

    return q from loader and set as inputs default value

--- app/root.tsx
+++ app/root.tsx
@@ -27,7 +27,7 @@ export const loader = async ({
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return json({ contacts });
+  return json({ contacts, q });
 };
 
 export const action = async () => {
@@ -37,7 +37,7 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
 
   return (
@@ -56,6 +56,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
